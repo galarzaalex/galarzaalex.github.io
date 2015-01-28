c23f0669da482fac47dae6f7ab22b4118786ea9f
galarzamellon <galarza@hope.edu>
2015-01-28 13:54:40 -0500

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 
     var timeline = new VMM.Timeline();
-    timeline.init("data.json");
+    timeline.init("https://docs.google.com/spreadsheets/d/1PIHXuEypx7-zqbhYXgmd3Y1D6M7u4oz0wImhflgw_Rc/pubhtml");
 
 });
